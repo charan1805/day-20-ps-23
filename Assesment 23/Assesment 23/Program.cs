c95c8ddde983c3f25b9764802868d13cf21c91ab
charan1805 <sreecharanmudhiraj@gmail.com>
2023-08-09 17:46:27 +0530

    First Class

---
+++ Assesment 23/Assesment 23/Program.cs
@@ -0,0 +1,29 @@
+﻿using AssesmentLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Login Page");
+                LoginOperator.login();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!! " + ex.Message);
+            }
+            finally { Console.ReadKey(); }
+
+
+
+        }
+    }
+}
